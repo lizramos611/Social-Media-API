e0ef4b704ab8d7202c16323ed3a5b1968ccadc06
lizramos611 <liz.ramos@aim.com>
2023-03-20 15:10:53 -0400

    adding in thoughts and reaction in js

--- models/thoughts.js
+++ models/thoughts.js
@@ -9,5 +9,23 @@ const thoughtSchema = new Schema ({
     created: {
         type: Date,
         default: Date.now
-    }
+    },
+    username: {
+        type:String,
+        required: true
+    },
+    {
+        reactions: [reactionSchema],
+    },{
+    toJSON: {
+        getters: true,
+        virtuals: true
+    }},{
+        id: false
+    },
+
+});
+
+thoughtSchema.virtual('reaction').get(function () {
+    return this.reactions.length;
 })
