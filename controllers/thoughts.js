cfa74960f0510be0d2131969d5bd7f06089de08a
lizramos611 <liz.ramos@aim.com>
2023-03-21 09:05:13 -0400

    trying to fix bugs

--- controllers/thoughts.js
+++ controllers/thoughts.js
@@ -1,20 +1,21 @@
-const {User, Thoughts, Reactions, thoughts, reactions} = require('../models');
+const { User, Thoughts} = require('../models');
 
 module.exports = {
     getThoughts (req, res) {
-        Thoughts.find()
-        .then((thought) => {
-            res.json(thought)
-        })
-    }
-}
+        Thoughts.find({})
+        .then((thought) => 
+            res.json(thought))
+            .catch((err) => res.status(500).json(err))
+        },
+    
+
 
 getSingleThought (req, res) {
     Thoughts.findOne({_id: req.params._id})
     .then((thought) =>
     !thought ? res.status(404).json({message: 'doesnt exist'})
     :res.json(thought))
-}
+},
 
 createThought(req, res) {
     Thoughts.create(req.body).then((thought) => res.json(thought))
@@ -22,16 +23,16 @@ createThought(req, res) {
         console.log(err);
         res.status(500).json(err);
       });
-}
+},
 
 updateThought(req, res) {
     Thoughts.findOneAndUpdate(
         {_id: reactions.params._id},
         {runValidators: true, new: true}
     ) .then((thought) => !thought?res.status(404).json:res.json(thought))
-}
+},
 
 deleteThought(req, res) {
     Thoughts.findOneAndDelete({_id: req.params._id})
     .then((thought) => !thought?res.status(404).json:res.json(thought))
-}
+}};
